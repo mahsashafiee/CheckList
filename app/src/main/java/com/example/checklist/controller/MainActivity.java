f7f81c5cc64cc9e440d0c3a2dd88c29bc058b1ba
Mahsa <mahsashafiee6@gmail.com>
2019-10-01 22:59:18 +0330

    fourth commit: add database for both task and user model, change task state value from string to integer, declare two method for convert enum to int and convert int to enum, editing ui, store all the data in database. create a POJO for hashing passwords, add two queries for delete one and all task. fifth commit: create an admin user who access all user's data and can edit or delete any of user's tasks. sixth commit: add logout option for users with startActivity and adding intent'flag: FLAG_ACTIVITY_CLEAR_TOP.

--- app/src/main/java/com/example/checklist/controller/MainActivity.java
+++ app/src/main/java/com/example/checklist/controller/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.checklist.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Intent newIntent(Context context){
+
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
